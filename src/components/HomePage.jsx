5c735e5775f6e3f5d51a9bc7ec9763355b582c4c
Kuba Goły <kuba@MacBook-Air-Jakub.local>
2023-11-17 13:00:24 +0100

    deleted padding

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,7 +10,10 @@ import { useAuthContext } from "../hooks/useAuthContext";
 import { Grid } from "@mui/joy";
 import LeaguesList from "./LeaguesList";
 import SwitchPanels from "./SwitchPanels";
-
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import { RedirectButtonOnRegisterAndLoginPages } from "./RedirectButtonOnRegisterAndLoginPages";
 export default function HomePage({
   clubs,
   showClubList,
@@ -100,6 +103,8 @@ export default function HomePage({
                         .slice(1)} 👑`
                     : `Welcome on useScore, choose the league!`}
                 </h1>
+
+                {!user && <LogoutButtonStackOnHomePage />}
                 {user ? (
                   <SwitchPanels
                     yourClubsList={yourClubsList}
@@ -119,5 +124,40 @@ export default function HomePage({
         </Grid>
       </Box>
     </>
+  );
+}
+
+function LogoutButtonStackOnHomePage() {
+  return (
+    <Stack spacing={2} sx={{ margin: "40px", textAlign: "center" }}>
+      <Link to="/login">
+        <Button
+          variant="outlined"
+          color="success"
+          sx={{
+            paddingInline: "100px",
+            marginTop: "20px",
+            border: "1px dashed #16DB65",
+            color: "white",
+          }}
+        >
+          Login
+        </Button>
+      </Link>
+      <Link to="/register">
+        <Button
+          variant="outlined"
+          color="success"
+          sx={{
+            paddingInline: "87px",
+            marginTop: "10px",
+            border: "1px dashed #16DB65",
+            color: "white",
+          }}
+        >
+          Register
+        </Button>
+      </Link>
+    </Stack>
   );
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -25,116 +25,118 @@ export default function Register() {
   };
 
   return (
-    <Box style={{ textAlign: "center", width: "100%" }}>
-      {error && (
-        <Alert
-          sx={{ marginTop: "20px" }}
-          severity="error"
-          variant="outlined"
-          onClose={() => {}}
-        >
-          {error}
-        </Alert>
-      )}
-      <Card
-        sx={{
-          display: "inline-block",
-          maxWidth: 345,
-          minWidth: 300,
-          backgroundColor: "#0D2818",
-          border: "1px dashed #16DB65",
-          marginTop: "40px",
-        }}
-      >
-        <CardMedia
+    <>
+      <Box style={{ textAlign: "center", width: "100%" }}>
+        {error && (
+          <Alert
+            sx={{ marginTop: "20px" }}
+            severity="error"
+            variant="outlined"
+            onClose={() => {}}
+          >
+            {error}
+          </Alert>
+        )}
+        <Card
           sx={{
-            height: 230,
-            width: 400,
-            borderBottom: "1px dashed #16DB65",
+            display: "inline-block",
+            maxWidth: 345,
+            minWidth: 300,
+            backgroundColor: "#0D2818",
+            border: "1px dashed #16DB65",
+            marginTop: "40px",
           }}
-          image="RegisterIMG.avif"
-          title="Login Cover"
-        />
-        <form onSubmit={handleRegister}>
-          <CardContent>
-            <Stack spacing={3}>
-              <label
-                htmlFor="email"
+        >
+          <CardMedia
+            sx={{
+              height: 230,
+              width: 400,
+              borderBottom: "1px dashed #16DB65",
+            }}
+            image="RegisterIMG.avif"
+            title="Login Cover"
+          />
+          <form onSubmit={handleRegister}>
+            <CardContent>
+              <Stack spacing={3}>
+                <label
+                  htmlFor="email"
+                  style={{
+                    fontFamily: "roboto",
+                    color: "white",
+                    fontSize: "14px",
+                  }}
+                >
+                  EMAIL{" "}
+                  <input
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    style={{
+                      all: "unset",
+                      height: "25px",
+                      backgroundColor: "white",
+                      borderBottom: "1px solid #16DB65",
+                      marginTop: "15px",
+                      borderRadius: "3px",
+                      color: "black",
+                    }}
+                  />
+                </label>
+                <label
+                  htmlFor="password"
+                  style={{
+                    fontFamily: "roboto",
+                    color: "white",
+                    fontSize: "14px",
+                  }}
+                >
+                  PASSWORD{" "}
+                  <input
+                    autoComplete="on"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    style={{
+                      all: "unset",
+                      height: "25px",
+                      borderBottom: "1px solid #16DB65",
+                      color: "black",
+                      backgroundColor: "white",
+                      marginTop: "10px",
+                      borderRadius: "3px",
+                    }}
+                  />
+                </label>
+              </Stack>
+            </CardContent>
+            <CardActions>
+              <button
+                disabled={isLoading}
+                onClick={(e) => {
+                  console.log("Clicked");
+                  e.target.style.color = "white";
+                }}
+                size="small"
                 style={{
-                  fontFamily: "roboto",
-                  color: "white",
+                  fontWeight: "450",
                   fontSize: "14px",
+                  fontFamily: "roboto",
+                  color: "#16DB65",
+                  backgroundColor: "rgba(255, 255, 255, 0)",
+                  border: "none",
+                  padding: "8px",
                 }}
               >
-                EMAIL{" "}
-                <input
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  style={{
-                    all: "unset",
-                    height: "25px",
-                    backgroundColor: "white",
-                    borderBottom: "1px solid #16DB65",
-                    marginTop: "15px",
-                    borderRadius: "3px",
-                    color: "black",
-                  }}
-                />
-              </label>
-              <label
-                htmlFor="password"
-                style={{
-                  fontFamily: "roboto",
-                  color: "white",
-                  fontSize: "14px",
-                }}
-              >
-                PASSWORD{" "}
-                <input
-                  autoComplete="on"
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  style={{
-                    all: "unset",
-                    height: "25px",
-                    borderBottom: "1px solid #16DB65",
-                    color: "black",
-                    backgroundColor: "white",
-                    marginTop: "10px",
-                    borderRadius: "3px",
-                  }}
-                />
-              </label>
-            </Stack>
-          </CardContent>
-          <CardActions>
-            <button
-              disabled={isLoading}
-              onClick={(e) => {
-                console.log("Clicked");
-                e.target.style.color = "white";
-              }}
-              size="small"
-              style={{
-                fontWeight: "450",
-                fontSize: "14px",
-                fontFamily: "roboto",
-                color: "#16DB65",
-                backgroundColor: "rgba(255, 255, 255, 0)",
-                border: "none",
-                padding: "8px",
-              }}
-            >
-              REGISTER
-            </button>
-          </CardActions>
-        </form>
-      </Card>
-      <RedirectButtonOnRegisterAndLoginPages text="or login" to="/login" />
-    </Box>
+                REGISTER
+              </button>
+            </CardActions>
+          </form>
+        </Card>
+        <RedirectButtonOnRegisterAndLoginPages text="or login" to="/login" />
+      </Box>
+    </>
   );
 }
