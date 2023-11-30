b51ece2a4a5aefc6c16164f081c9b902cae62bb5
Kuba Goły <kuba@MacBook-Air-Jakub.local>
2023-11-30 09:38:15 +0100

    made props hook

--- src/hooks/useProps.jsx
+++ src/hooks/useProps.jsx
@@ -3,5 +3,8 @@ import { Context } from "../context/Context";
 
 export default function useProps() {
   const context = useContext(Context);
+  if (!context) {
+    throw Error("useContext must be used inside an ContextProvider");
+  }
   return context;
 }
