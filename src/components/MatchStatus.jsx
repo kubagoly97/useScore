38adbda08b499691fa2ac989080ffb748f7f2f04
Kuba Goły <kuba@MacBook-Air-Jakub.local>
2024-01-30 13:22:18 +0100

    updated footballbar and favourites matches

--- src/components/MatchStatus.jsx
+++ src/components/MatchStatus.jsx
@@ -18,7 +18,13 @@ export function MatchStatus({
           (match.match_status == "Postponed" && postponed) ||
           (match.match_status == "Half Time" && halfTime) ||
           (match.match_status.length && (
-            <span style={{ fontSize: "22px", color: "white" }}>
+            <span
+              style={{
+                fontSize: favourites ? "18px" : "22px",
+                color: "white",
+                paddingLeft: favourites ? "18px" : "none",
+              }}
+            >
               <CircleIcon fontSize="small" sx={{ color: "red" }} />{" "}
               {match.match_status}'
             </span>
